9d80ca5f61cf75e155a6b210b9a71e4a9db92fa4
sakmoto <hongbonsak2@gmail.com>
2026-03-12 20:23:28 +0700

    commit

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [result, setResult] = useState("");
+  const [hasError, setHasError] = useState(false);
 
   /* You will need some function to handle the key pressed and button events */
+  function onA(event) {
+    setA(event.target.value);
+  }
+
+  function onB(event) {
+    setB(event.target.value);
+  }
+
+  function onCompute() {
+    const aNumber = Number(a);
+    const bNumber = Number(b);
+
+    if (a.trim() === "" || b.trim() === "" || Number.isNaN(aNumber) || Number.isNaN(bNumber)) {
+      setResult("Please enter valid numbers");
+      setHasError(true);
+      return;
+    }
+
+    setResult(String(aNumber + bNumber));
+    setHasError(false);
+  }
 
   return (
     <main>
@@ -18,8 +43,8 @@ function App() {
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input className={hasError ? "error" : ""} value={result} disabled />
+      <button onClick={onCompute}>Compute</button>
     </main>
   );
 }
--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
   /* You will need to  use  state to mnage the current image */
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   /* You will need to hanle the click on left and right button */
+  function onPreviousClick() {
+    setCurrentImageIndex((previousIndex) => {
+      if (previousIndex === 0) {
+        return images.length - 1;
+      }
+
+      return previousIndex - 1;
+    });
+  }
+
+  function onNextClick() {
+    setCurrentImageIndex((previousIndex) => {
+      if (previousIndex === images.length - 1) {
+        return 0;
+      }
+
+      return previousIndex + 1;
+    });
+  }
 
   /* You will need to manage the cases when we are on the last image or first image*/
+  if (!images || images.length === 0) {
+    return null;
+  }
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={onPreviousClick} />
 
       {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      <img src={images[currentImageIndex].src} alt={images[currentImageIndex].alt} className="slide" />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill className="arrow arrow-right" onClick={onNextClick} />
     </div>
   );
 };
